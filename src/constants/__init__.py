bd046b07553e8e5121a5568cf5740770f7e52d3b
mukesh1996-ds <mks.mukesh1996@gmail.com>
2024-09-30 15:11:51 +0530

    Data Ingestion

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -6,7 +6,7 @@ from datetime import datetime
 def get_current_time_stamp():
     return f"{datetime.now().strftime('%Y-%M-%d %H-%M-%S')}"
 
-CURRENT_TIME_STAPM=get_current_time_stamp()
+CURRENT_TIME_STAMP=get_current_time_stamp()
 
 # Data Set path 
 ROOT_DIR_KEY=os.getcwd()
@@ -23,3 +23,11 @@ RAW_DATA_DIR_KEY="raw.csv"
 TRAIN_DATA_DIR_KEY="train.csv"
 TEST_DATA_DIR_KEY="test.csv"
 
+# Data Transformation related variable
+
+DATA_TRANSFORMATION_ARTIFACT = "data_transformation"
+DATA_PREPROCCED_DIR = "procceor"
+DATA_TRANSFORMTION_PROCESSING_OBJ = "processor.pkl"
+DATA_TRANSFORM_DIR = "transformation"
+TRANSFORM_TRAIN_DIR_KEY = "train.csv"
+TRANSFORM_TEST_DIR_KEY = "test.csv"
